5c57cf73974489f1209d8ec19b1dbdb95f795596
Will <widiep@gmail.com>
2020-06-30 07:54:48 -0700

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 import resumePDF from '../assets/resume.pdf'
 
 const Nav = styled.nav`
@@ -29,6 +30,11 @@ const LeftNav = styled.div`
   flex-grow: 1;
 `
 
+const LogoImg = styled.img`
+  width: 4rem;
+`
+
+
 const RightNav = styled.div`
   display: flex;
 
@@ -42,7 +48,9 @@ class Navbar extends Component {
     return (
       <Nav>
           <LeftNav>
-            <Link to='/'>KIMMY INTERIOR DESIGNER</Link>
+            <Link to='/'>
+              <LogoImg src={logo} alt='logo' />
+            </Link>
           </LeftNav>
 
           <RightNav>
